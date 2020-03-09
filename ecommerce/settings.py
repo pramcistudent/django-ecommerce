3b9a0b9ad0b2cb20961c0c49216edd6447601d60
Pram Sanghani <pramod.s@hotmail.co.uk>
2020-03-09 19:51:39 +0000

    Added allowed host for Heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ps-ecommerce.herokuapp.com']
 
 
 # Application definition
